cec02991c4df9c6c8caff02f49cb8c4bfee3e4c0
kewal07 <kewal07@gmail.com>
2015-05-11 11:45:48 +0530

    shopping app

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('#enterItem').keydown(function(e) {
+    	if (e.keyCode == 13) {
+        	e.preventDefault();
+        	var item = $('#enterItem').val();
+       		if(item.length > 0){
+       			$('#items ul').append('<li class="liItem"><i class="fa fa-check"></i><span class="spanItem"></span><i class="fa fa-times"></i></li>');
+       			$('.spanItem').last().text(item);
+       			$('#enterItem').val('');
+       		}
+    	}
+	});
+  $(document).on('click', '.fa-check', function() {
+    var item = $(this).children();
+    $(this).next().css('text-decoration','overline underline line-through');
+  });
+	$(document).on('click', '.fa-times', function() {
+    $(this).parent().remove();
+  });
+});
